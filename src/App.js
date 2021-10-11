363417fc50b097dd8397167801721632cb040dbb
isyxZA <49752811+isyxZA@users.noreply.github.com>
2021-10-11 16:59:20 +0200

    commit .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import Tasks from './components/Tasks';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
+	const [ showAddTask, setShowAddTask ] = useState(false)
 	const [ tasks, setTasks ] = useState([
 		{
 			id: 1,
@@ -18,32 +20,40 @@ const App = () => {
 		},
 		{
 			id: 3,
-			text: 'Food Shooping',
+			text: 'Food Shoping',
 			day: 'Tomorrow at 9:00pm',
 			reminder: false
 		}
-	]);
+	])
+
+	//Add Task
+	const addTask = (task) => {
+		const id = Math.floor(Math.random() * 10000) + 1
+		const newTask = { id, ...task }
+		setTasks([ ...tasks, newTask ])
+	}
 
 	//Delete Task
 	const deleteTask = (id) => {
-		setTasks(tasks.filter((task) => task.id !== id));
-	};
+		setTasks(tasks.filter((task) => task.id !== id))
+	}
 
 	//Reminder toggle
 	const toggleReminder = (id) => {
-		setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)));
-	};
+		setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)))
+	}
 
 	return (
 		<div className="container">
-			<Header />
+			<Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+			{showAddTask && <AddTask onAdd={addTask} />}
 			{tasks.length > 0 ? (
 				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
 			) : (
 				'No Tasks To Display'
 			)}
 		</div>
-	);
-};
+	)
+}
 
-export default App;
+export default App
